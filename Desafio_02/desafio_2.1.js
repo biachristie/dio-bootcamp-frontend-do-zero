155272199ca60b50110a32910b6e463a0ef72829
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2024-12-11 15:58:36 -0300

    add Desafio 02

---
+++ Desafio_02/desafio_2.1.js
@@ -0,0 +1,13 @@
+// Capturando os nomes dos brinquedos
+let brinquedo1 = "Boneca";
+let brinquedo2 = "Carrinho";
+let brinquedo3 = "Lego";
+
+// Imprimindo a lista
+console.log("<ul>");
+console.log(`<li>${brinquedo1}</li>`);
+
+// TODO: Preencha o código abaixo com as variáveis que representam os nomes dos brinquedos
+console.log(`<li>${brinquedo2}</li>`);
+console.log(`<li>${brinquedo3}</li>`);
+console.log("</ul>");
---
+++ Desafio_02/desafio_2.2.js
@@ -0,0 +1,9 @@
+const cor = "red";
+const tamanhoFonte = "16px";
+const nomeBrinquedo = "Carrinho de Controle Remoto";
+
+// TODO: Construa a tag <h1> com o estilo CSS, completando com as variáveis representando cada estilização
+const resultado = `<h1 style="color: ${cor}; font-size: ${tamanhoFonte};">${nomeBrinquedo}</h1>`;
+
+// Exibindo o resultado no console
+console.log(resultado);
