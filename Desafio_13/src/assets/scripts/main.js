f4d92ab23730c7b300355760b5fe99624b86a784
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2024-12-15 19:07:22 -0300

    add carousel to main page at Desafio 13

--- Desafio_13/src/assets/scripts/main.js
+++ Desafio_13/src/assets/scripts/main.js
@@ -1,3 +1,23 @@
-import { animateCardHover } from "./card-hover-animation.js";
+import { animateCardHover } from "./card-animation.js";
 
 animateCardHover();
+
+const selectCarouselItem = (selectedButton) => {
+    const selectedItem = selectedButton.target.id;
+    const carousel = document.querySelector(".s-cards-carousel");
+
+    const transform = carousel.style.transform;
+    const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+    const rotateYDeg = -120 * (Number(selectedItem) - 1);
+
+    const newTransform = transform.replace(rotateY[0], `rotateY(${ rotateYDeg }deg)`);
+    carousel.style.transform = newTransform;
+
+    const activeButtonElement = document.querySelector(".s-controller__button--active");
+    activeButtonElement.classList.remove("s-controller__button--active");
+    selectedButton.target.classList.add("s-controller__button--active");
+}
+
+document.getElementById("1").onclick = selectCarouselItem;
+document.getElementById("2").onclick = selectCarouselItem;
+document.getElementById("3").onclick = selectCarouselItem;
